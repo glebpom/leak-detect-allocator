f951de15e8714c1e6e6d7f148594875bda081be4
Gleb Pomykalov <gleb@exogress.com>
2021-05-15 23:25:13 +0300

    std

--- src/lib.rs
+++ src/lib.rs
@@ -104,8 +104,7 @@ where
                     Some(x) => {
                         use std::fmt::Write;
                         write!(&mut symbol_buf, "{}", x).ok();
-                        write!(&mut symbol_buf, "; {:?}", symbol.addr()).ok();
-                        write!(&mut symbol_buf, "; {:?}", symbol.filename_raw()).ok();
+                        write!(&mut symbol_buf, "; {:?}", symbol.filename()).ok();
                         write!(&mut symbol_buf, "; {:?}", symbol.lineno()).ok();
                         got = true;
                     }
